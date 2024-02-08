b4b30328dd45515fafb346930a50bd58ec21ef97
sanghui <tkdgml719@gamil.com>
2024-02-08 13:48:53 +0900

    연습 커밋

--- 17_spring_mvc/src/main/java/com/application/mvc/chapter01_thymeleaf/ThymeleafController.java
+++ 17_spring_mvc/src/main/java/com/application/mvc/chapter01_thymeleaf/ThymeleafController.java
@@ -1,4 +1,4 @@
-package com.application.mvc.chapter01_thymeleaf;
+	package com.application.mvc.chapter01_thymeleaf;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
