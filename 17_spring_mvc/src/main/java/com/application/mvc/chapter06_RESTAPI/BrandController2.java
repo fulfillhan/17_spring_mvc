15dacab3829601bec7505c6eb6f99104b8678dc2
sanghui <tkdgml719@gamil.com>
2024-02-26 12:13:02 +0900

    2024.02.26 연습 커밋

--- 17_spring_mvc/src/main/java/com/application/mvc/chapter06_RESTAPI/BrandController2.java
+++ 17_spring_mvc/src/main/java/com/application/mvc/chapter06_RESTAPI/BrandController2.java
@@ -1,14 +1,23 @@
 package com.application.mvc.chapter06_RESTAPI;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.application.mvc.data.BrandDTO;
+import com.mysql.cj.log.Log;
+
 @Controller
 class BrandTestController02{
-	@GetMapping("/RESTAPI/view")
+	@GetMapping("/RESTAPI/view02")
 	public String view() {
 		return "chapter06_RESTAPI/brand2";
 	}
@@ -24,9 +33,26 @@ public class BrandController2 {
 	
 	// Brand 데이터 전체 요청
 	@GetMapping
-	public void getBrandList() {
+	public List<BrandDTO> getBrandList() {
+		return brandDAO.getBrandList();
 		
 	}
-		
+	
+	@GetMapping("#{brandId}")
+	public BrandDTO getBrandDetail(@PathVariable("brandId") long brandId) {
+		return brandDAO.getBrandDetail(brandId);
+	}
+	
+	@PostMapping
+	public void  createBrand(@RequestBody BrandDTO brandDTO) {
+		brandDAO.createBrand(brandDTO);
+	}
+	
+	@PutMapping
+	public void updateBrand(@RequestBody BrandDTO brandDTO) {
+		 brandDAO.updateBrand(brandDTO);
+	}
+	
+	
 
 }
